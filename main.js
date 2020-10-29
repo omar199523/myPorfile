f8c09c4055467ec9a9a73208d3287023afe8f704
omar199523 <model199523@gmail.com>
2020-10-29 21:15:21 +0300

    i create my Serveis and styling this

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 window.onscroll=()=>{
     document.getElementById("header").classList.toggle("haderScroll",window.scrollY>0);
 }
+document.getElementById("toggle").onclick=()=>{
+    document.getElementById("toggle").classList.toggle("toggle")
+}
+
